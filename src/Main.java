7833e39574c4a1b71953b42e88530ea5c6e21528
Dori <qarkchanjin@gmail.com>
2023-05-31 21:31:58 +0900

    JDBC 구성 중

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import JDBC.ConnectMyDB;
+
+import java.sql.Date;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectMyDB connectMyDB = new ConnectMyDB();
+        connectMyDB.insertNotice(new Date(System.currentTimeMillis()),"chanjin","testNotice","content2",0);
+        connectMyDB.disConnectMyDB();
     }
 }
