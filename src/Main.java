9f1fde4862d1c1932ddf18b72efc87b55a5b8d30
Dori <qarkchanjin@gmail.com>
2023-06-07 22:11:12 +0900

    컨트롤러, entity 클래스다이어그램대로 세팅

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import JDBC.ConnectMyDB;
+import NoticeManage.Notice;
+import NoticeManage.NoticeManagement;
 
 import java.sql.Date;
 import java.sql.SQLException;
@@ -6,7 +8,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ConnectMyDB connectMyDB = new ConnectMyDB();
-        connectMyDB.insertNotice(new Date(System.currentTimeMillis()),"chanjin1","testNotice1","content3",0);
+        NoticeManagement noticeManagement = new NoticeManagement();
+        Notice notice = noticeManagement.createNotice(new Date(System.currentTimeMillis()),"chanjin1","testNotice1","content3",0);
+        connectMyDB.insertNotice(notice);
         connectMyDB.disConnectMyDB();
     }
 }
