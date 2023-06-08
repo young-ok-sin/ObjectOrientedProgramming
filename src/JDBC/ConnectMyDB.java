583c69499729c438e18623bf6a95c8badc35eb6e
sorim0418 <solimsa11@naver.com>
2023-06-08 23:31:03 +0900

    마스터에서 긁어 온 것들 저장

--- src/JDBC/ConnectMyDB.java
+++ src/JDBC/ConnectMyDB.java
@@ -8,15 +8,6 @@ public class ConnectMyDB{
     String url = "jdbc:mysql://localhost:3306/MyProject";
     String option = "?useUnicode=true&characterEncoding=euckr";
     String userName = "user";
-
-    public ResultSet getResultSet() {
-        return resultSet;
-    }
-
-    public void setResultSet(ResultSet resultSet) {
-        this.resultSet = resultSet;
-    }
-
     String password = "1234";
 
     Connection connection = null; //DB 연결
@@ -28,6 +19,7 @@ public class ConnectMyDB{
         url = url + option;
         connection = DriverManager.getConnection(url, userName, password);
         statement = connection.createStatement();
+        System.out.println("DB에 연결되었습니다.");
     }
     public void disConnectMyDB() throws SQLException {
         if(resultSet != null) {
@@ -41,33 +33,21 @@ public class ConnectMyDB{
         }
     }
 
-    public void insertNotice(Notice notice) throws SQLException {
-        String query = "INSERT INTO notice VALUES (DEFAULT, ?, ?, ?, ?, ?)";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setDate(1,notice.getDate());
-        pstmt.setString(2, notice.getWriter());
-        pstmt.setString(3, notice.getTitle());
-        pstmt.setString(4,notice.getContent());
-        pstmt.setInt(5, notice.getResult());
-        pstmt.executeUpdate();
+    public boolean insertNotice(Notice notice) {
+        boolean result = true;
+        try {
+            String query = "INSERT INTO notice VALUES (DEFAULT, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setDate(1, notice.getDate());
+            pstmt.setString(2, notice.getWriter());
+            pstmt.setString(3, notice.getTitle());
+            pstmt.setString(4, notice.getContent());
+            pstmt.setInt(5, notice.getResult());
+            pstmt.executeUpdate();
+        }catch (SQLException e) {
+            result = false;
+            System.out.println("insert Error");
+        }
+        return result;
     }
-
-    public ResultSet inquiryOffice() throws SQLException {
-        System.out.println("office select 들어옴");
-        String query = "SELECT * FROM rentaloffice";
-        resultSet = statement.executeQuery(query);
-        return resultSet;
-    }
-    public ResultSet selectBicycleByOfficeID(String oID) throws SQLException {
-        System.out.println("bicycle select 들어옴");
-        String query = "SELECT * FROM bicycle where officeID=?";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setString(1, oID);
-
-        resultSet = pstmt.executeQuery();
-        System.out.println(query);
-       // resultSet = statement.executeQuery(query);
-        return resultSet;
-    }
-
 }
