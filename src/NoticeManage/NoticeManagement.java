b239860d589e5ac8cb4b31f9a671201a23ade980
Dori <qarkchanjin@gmail.com>
2023-06-09 21:36:42 +0900

    실패, 성공 jsp 공통으로 만들었음, 조회 페이징하는 쿼리문 작성 및 테스트 완료.

--- src/NoticeManage/NoticeManagement.java
+++ src/NoticeManage/NoticeManagement.java
@@ -4,29 +4,32 @@ import JDBC.ConnectMyDB;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NoticeManagement {
 
-    private Notice notice = null;
+    private Notice noticeInfo = null;
     private ConnectMyDB connectMyDB = null;
 
     public NoticeManagement() throws SQLException, ClassNotFoundException {
-        notice = new Notice();
+        noticeInfo = new Notice();
         connectMyDB = new ConnectMyDB();
     }
 
     public Notice getNotice(Date date, String writer, String title, String content, int result) {
-            notice.setDate(date);
-            notice.setWriter(writer);
-            notice.setTitle(title);
-            notice.setContent(content);
-            notice.setResult(result);
-        return notice;
+        noticeInfo.setDate(date);
+        noticeInfo.setWriter(writer);
+        noticeInfo.setTitle(title);
+        noticeInfo.setContent(content);
+        noticeInfo.setResult(result);
+        return noticeInfo;
     }
+
     public boolean insertNotice(Notice notice) throws SQLException {
-        boolean result = true;
+        boolean result = false;
         try {
             String query = "INSERT INTO notice VALUES (DEFAULT, ?, ?, ?, ?, ?)";
             PreparedStatement pstmt = connectMyDB.getConnection().prepareStatement(query);
@@ -36,15 +39,41 @@ public class NoticeManagement {
             pstmt.setString(4, notice.getContent());
             pstmt.setInt(5, notice.getResult());
             pstmt.executeUpdate();
-        }catch (SQLException e) {
-            result = false;
+            result = true;
+        } catch (SQLException e) {
             System.out.println("insert Error");
         }
         connectMyDB.disConnectMyDB();
         return result;
     }
-    public List<Notice> inquiryNoticeAll (int pageNum) {
-        return null; //임시로 만듦
+
+    public List<Notice> inquiryNoticeAll(int pageNum) {
+        String query = "SELECT * FROM notice ORDERS LIMIT ?,?";
+        int pageSize = 10; // 페이지당 행의 수
+        int pageNumber = pageNum; // 페이지 번호
+        List<Notice> list = new ArrayList<>();
+        try {
+            PreparedStatement pstmt = connectMyDB.getConnection().prepareStatement(query);
+            pstmt.setInt(1, (pageNumber - 1) * pageSize); // OFFSET의 값 설정
+            pstmt.setInt(2, pageSize); // LIMIT의 값 설정
+
+
+            ResultSet resultset = pstmt.executeQuery();
+
+            while(resultset.next()) {
+                Notice noticeInfo = new Notice();
+                noticeInfo.setDate(resultset.getDate("date"));
+                noticeInfo.setWriter(resultset.getString("writer"));
+                noticeInfo.setTitle(resultset.getString("title"));
+                noticeInfo.setContent(resultset.getString("content"));
+                noticeInfo.setResult(resultset.getInt("result"));
+                list.add(noticeInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error selectNotice");
+        }
+        return list;
     }
+
 
 }
---
+++ web/ResultPage/Success.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: qarkc
+  Date: 2023-06-09(009)
+  Time: 오후 7:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" href="../MainHeader/MainHeader.css">
+    <link rel="stylesheet" href="Result.css">
+    <title>Success</title>
+</head>
+<body>
+<jsp:include page="../MainHeader/MainHeader.jsp"></jsp:include>
+<div class = "result">
+    <img src="../img/bicycle_oos.png">
+    <span>성공</span>
+    <button class = "back-to-MainPage" onclick="location.href='../MainPage/MainPage.jsp'">메인으로 돌아가기</button>
+</div>
+</body>
+</html>
